a42b5240c88840df208672a47fc46801e8197ff0
Zachary Wu <andywu03242000@gmail.com>
2024-01-09 19:31:16 +0800

    ADD: Initialize vue3+vite

---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar"></div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<style scoped>
+.navbar {
+
+}
+</style>
+  
---
+++ src/components/home/Grid.vue
@@ -0,0 +1,14 @@
+<template>
+   <div class="grid"></div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<style scoped>
+.grid {
+   
+}
+</style>
